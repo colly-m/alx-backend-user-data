383bea91f18b872a4681235b4a12e1c4555331d4
colly-m <sindanicolly@gmail.com>
2024-05-30 21:32:26 +0000

    Logger def update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-"""Module to return an obfuscated log message"""
+"""Module for a filtered logger"""
 import re
+import logging
+from typing import List
+from os import environ
+
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -12,5 +17,36 @@ def filter_datum(fields, redaction, message, separator):
         message: string reping a log line
         separator: string reping characters for separating fields
     """
-    pattern = "|".join(f"(?<={field}=)[^{separator}]+" for field in fields)
-    return re.sub(pattern, redaction, message)
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """Returns logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message,
+                            self.SEPARATOR)
