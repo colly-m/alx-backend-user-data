a30561ccfe0de2f4aa56ae53c39967d7a3dcbfed
colly-m <sindanicolly@gmail.com>
2024-06-13 20:06:30 +0000

    Basic flask app update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 """Module to define a method that take password and retun bytes"""
 import bcrypt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """Function to generate a salt to turn the input args into bytes"""
-    salt = bcrypt.gensalt()
-    hash_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+class Auth:
+    """A class to interact with authentication db"""
 
-    return hash_pwd
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(password: str) -> bytes:
+        """Function to generate a salt to turn the input args into bytes"""
+        salt = bcrypt.gensalt()
+        hash_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return hash_pwd
+
+    def register_user(self, email: str, password: str) -> User:
+        """Function to register a new user with email and password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+
+        hash_pwd = self._hash_password(password)
+
+        n_user = User(email, hashed_pwd)
+        self._db.save_user(n_user)
+
+        return n_user
